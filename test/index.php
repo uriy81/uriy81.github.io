276a4a22ec90b334d3e2d4d56fc27c39d61f383d
Yuriy Vasiliev <62181793+uriy81@users.noreply.github.com>
2020-11-25 13:18:43 +0200

    Create index.php

---
+++ test/index.php
@@ -0,0 +1,23 @@
+<?php
+header('Location: instagram://user?username=postelnoe.odnotonnoe',301);
+header('refresh: 1');
+?>
+
+
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="utf-8">
+    
+    <title></title>
+    <link rel="stylesheet" href="">
+    
+  </head>
+  <body>
+
+    <main>
+     
+    </main>
+
+  </body>
+</html>
